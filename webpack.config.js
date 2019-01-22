fbfaff7776d47afaa70d3b5f24e4f6ff15948f39
sneakyfildy <poleff@yandex.ru>
2019-01-22 17:48:43 +0300

    firebase+heroku 2

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test' });
+    require('dotenv').config({path: '.env.test'});
 } else if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config({ path: '.env.development' });
+    require('dotenv').config({path: '.env.development'});
 }
 
 module.exports = (env) => {
@@ -53,14 +53,6 @@ module.exports = (env) => {
                 args: [`--APP_MODE=${mode}`],
                 script: './server/server.js',
                 watch: [path.resolve('./dist'), './server/'],
-            }),
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
             })
         ],
     };
